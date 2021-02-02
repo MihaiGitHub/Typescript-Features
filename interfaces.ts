5f358d947858a43e32516451566445b820c3ba2d
Mihai Smarandache <mihai.sanfran@gmail.com>
2021-02-01 17:19:59 -0700

    Update interface with function

--- interfaces.ts
+++ interfaces.ts
@@ -2,12 +2,16 @@ interface Vehicle {
   name: string;
   year: number;
   broken: boolean;
+  summary(): string;
 }
 
 const oldCivic = {
   name: "civic",
   year: 2000,
   broken: true,
+  summary(): string {
+    return `Name: ${this.name}`;
+  },
 };
 
 const printVehicle = (vehicle: Vehicle): void => {
