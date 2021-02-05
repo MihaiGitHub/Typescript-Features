c08c20798c456195d9e85cf95175672daa13f2c3
Mihai Smarandache <mihai.sanfran@gmail.com>
2021-02-04 21:43:58 -0700

    Class inheritance

---
+++ classes.ts
@@ -0,0 +1,21 @@
+class Vehicle {
+  drive(): void {
+    console.log("chugga chugga");
+  }
+  honk(): void {
+    console.log("beep");
+  }
+}
+
+// Inheritance
+class Car extends Vehicle {
+  // override drive method;
+  drive(): void {
+    console.log("vroom");
+  }
+}
+
+const car = new Car();
+
+car.drive();
+car.honk();
