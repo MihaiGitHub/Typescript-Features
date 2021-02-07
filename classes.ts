bcaf6c8ab3e06e403f4e95fc3e8e7bc6c0eb6746
Mihai Smarandache <mihai.sanfran@gmail.com>
2021-02-06 19:37:16 -0700

    Add class contructors

--- classes.ts
+++ classes.ts
@@ -1,11 +1,24 @@
 class Vehicle {
-  public honk(): void {
+  // take first argument that comes in and assign as an instance variable of color
+  constructor(public color: string) {}
+
+  protected honk(): void {
     console.log("beep");
   }
 }
 
+const vehicle = new Vehicle("orange");
+console.log(vehicle.color);
+
 // Inheritance
 class Car extends Vehicle {
+  // when calling a constructor in a child class, the constructor of the parent must be
+  // called as well
+  constructor(public wheels: number, color: string) {
+    // reference to the constructor method in the parent
+    super(color);
+  }
+
   // override drive method;
   private drive(): void {
     console.log("vroom");
@@ -16,7 +29,7 @@ class Car extends Vehicle {
   }
 }
 
-const car = new Car();
+// since it extends vehicle, must pass in a string color
+const car = new Car(4, "red");
 
 car.startDrivingProcess();
-car.honk();
